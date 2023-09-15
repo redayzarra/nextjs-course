2177770fd358dc9b62f2c30ddf6dad688a476345
ReDay Zarra <redayzarra@gmail.com>
2023-09-15 16:09:54 -0400

    Creating an object using PUSH request

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -7,3 +7,13 @@ export function GET(request: NextRequest) {
     { id: 3, name: "Mark" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required." }, { status: 400 });
+  }
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
