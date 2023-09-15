79d41ebed37029051ff77cb27a3af01517896334
ReDay Zarra <redayzarra@gmail.com>
2023-09-15 15:08:50 -0400

    Learning next router for programmatic navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const NewUserPage = () => {
+  const router = useRouter();
   return (
-    <div>NewUserPage</div>
-  )
-}
+    <button className="btn btn-primary" onClick={() => router.push("/users")}>
+      Create
+    </button>
+  );
+};
 
-export default NewUserPage
+export default NewUserPage;
