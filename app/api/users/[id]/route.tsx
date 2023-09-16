cac3e271e94691f0b8451924b7773c4eee6239c7
ReDay Zarra <redayzarra@gmail.com>
2023-09-16 15:49:52 -0400

    Using Prisma to update users

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -20,7 +20,7 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
@@ -29,18 +29,34 @@ export async function PUT(
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  if (params.id > 10) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!user) {
     return NextResponse.json({ error: "User not found." }, { status: 404 });
   }
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export function DELETE(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  if (params.id > 10) {
+  if (parseInt(params.id) > 10) {
     return NextResponse.json({ error: "User not found." }, { status: 404 });
   }
 
