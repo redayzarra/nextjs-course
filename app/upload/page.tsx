509433af9639bf1d1c9751f730c3cf01251a3919
ReDay Zarra <redayzarra@gmail.com>
2023-09-17 10:47:45 -0400

    Uploding and showing images using Cloudinary

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,17 +1,41 @@
 "use client";
 
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="egm9lvrh">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={400}
+          alt="An image of a girl"
+        />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="egm9lvrh"
+        onUpload={(result, widget) => {
+          if (result.event != "success") return;
+
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
