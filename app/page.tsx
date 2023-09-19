6c26eb5494faacb89c36559f5a477c97ebe57364
ReDay Zarra <redayzarra@gmail.com>
2023-09-19 11:53:24 -0400

    Learning to add optimized local and remote images

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,26 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import girl from "@/public/images/shorthair.jpg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+      {/* <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
-      <ProductCard />
+      <ProductCard /> */}
+
+      <Image src={girl} alt="A female model with short hair" />
+
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="cover of React course"
+        width={300}
+        height={170}
+      />
     </main>
   );
 }
