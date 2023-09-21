2475226fe5fbffca0326a7e13c3a4d9a72d85c74
ReDay Zarra <redayzarra@gmail.com>
2023-09-21 06:14:45 -0400

    Suppressing sending emails

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -2,15 +2,15 @@ import WelcomeTemplate from "@/emails/WelcomeTemplate";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  await resend.emails.send({
-    from: "...",
-    to: "anotheremail...",
-    subject: "...",
-    react: null, // <WelcomeTemplate name="ReDay" />,
-  });
+// export async function POST() {
+//   await resend.emails.send({
+//     from: "...",
+//     to: "anotheremail...",
+//     subject: "...",
+//     react: null, // <WelcomeTemplate name="ReDay" />,
+//   });
 
-  return NextResponse.json({});
-}
+//   return NextResponse.json({});
+// }
